298529de355b9cd2ab540f344815d134d47ba2fd
taeng0204 <taeng02@icloud.com>
2022-11-29 11:29:21 +0900

    .

--- src/components/ReportPdf.js
+++ src/components/ReportPdf.js
@@ -7,23 +7,14 @@ const A4 = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
-  //padding: 12px;
 `
-const Img = styled.img`
-  width: inherit;
-`
+
 
 function ReportPdf({title}) {
   return (
     <A4>
-      <Img src={`${process.env.PUBLIC_URL}/graph.png`} alt="graph" />
       <h1>initCloud Document</h1>
       <h3>{title?title:"title"}</h3>
-      <span>Don't do that</span>
-      <h3>Statistics</h3>
-      <span>IaC 1: 0%</span>
-      <span>IaC 2: 12%</span>
-      <span>IaC 3: 999%</span>
     </A4>
   );
 }
